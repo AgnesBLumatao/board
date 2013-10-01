9a3730d8507dac14922fb62de46f0d0221a165bd
Agnes Lumatao <a.lumatao@klab.com>
2013-10-01 17:15:40 +0800

    Modify create_end view

--- app/views/thread/create_end.php
+++ app/views/thread/create_end.php
@@ -2,6 +2,6 @@
 <p class="alert alert-success">
 You successfully created.
 </p>
-<a href="<?php eh(url('thread/view', array('thread_id' => $thread->id))) ?>">
+<a href="<?php eh(url('thread/view', array('page'=>0,'thread_id' => $thread->id, 'username'=>$user['username'],'user_id'=>$user['user_id']))) ?>">
 &larr; Go to thread
 </a>
