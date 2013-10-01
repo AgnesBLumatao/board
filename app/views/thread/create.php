8c98762128df9fea1796916714d4777523d8a93f
Agnes Lumatao <a.lumatao@klab.com>
2013-10-01 17:15:28 +0800

    Modify create view

--- app/views/thread/create.php
+++ app/views/thread/create.php
@@ -1,5 +1,16 @@
+<table width="100%">
+<tr>
+<td>
 <h1>Create a thread</h1>
-<?php if ($thread->hasError() || $comment->hasError()): ?>
+</td>
+<td align="right">
+<h5>WELCOME <?php eh($user['username']) ?> <a class="btn btn-small btn-primary" href="<?php eh(url('thread/index')) ?>">LOG OUT</a></h5>
+</td>
+</tr>
+</table>
+
+<?php
+if ($thread->hasError() || $comment->hasError()){ ?>
 <div class="alert alert-block">
 <h4 class="alert-heading">Validation error!</h4>
 <?php if (!empty($thread->validation_errors['title']['length'])): ?>
@@ -7,13 +18,6 @@
 between
 <?php eh($thread->validation['title']['length'][1]) ?> and
 <?php eh($thread->validation['title']['length'][2]) ?> characters in length.
-</div>
-<?php endif ?>
-<?php if (!empty($comment->validation_errors['username']['length'])): ?>
-<div><em>Your name</em> must be
-between
-<?php eh($comment->validation['username']['length'][1]) ?> and
-<?php eh($comment->validation['username']['length'][2]) ?> characters in length.
 </div>
 <?php endif ?>
 <?php if (!empty($comment->validation_errors['body']['length'])): ?>
@@ -24,16 +28,21 @@ between
 </div>
 <?php endif ?>
 </div>
-<?php endif ?>
+<?php }
+elseif(isset($check) && $check==1){?>
+<div class="alert alert-block">
+<h3 class="alert-heading">Thread already exist!</h3>
+</div>
+<?php } ?>
+
 <form class="well" method="post" action="<?php eh(url('')) ?>">
 <label>Title</label>
 <input type="text" class="span2" name="title" value="<?php eh(Param::get('title')) ?>">
-<label>Your name</label>
-<input type="text" class="span2" name="username" value="<?php eh(Param::get('username')) ?>">
-
 <label>Comment</label>
 <textarea name="body"><?php eh(Param::get('body')) ?></textarea>
 <br />
+<?php if (isset($check) && $check!=1){?>
 <input type="hidden" name="page_next" value="create_end">
+<?php } ?>
 <button type="submit" class="btn btn-primary">Submit</button>
 </form>
