0c259374c48eb170ca1d6dcd5f951e9b2b77dc3e
Agnes Lumatao <a.lumatao@klab.com>
2013-10-02 15:44:48 +0800

    Login error for invalid account

--- app/views/thread/index.php
+++ app/views/thread/index.php
@@ -1,19 +1,34 @@
 <table width="100%">
-<tr>
-<td>
-<h1>LOG IN</h1>
-</td>
-<td align="right">
-<a class="btn btn-large btn-primary" href="<?php eh(url('thread/register')) ?>">SIGN UP</a>
-</td>
-</tr>
+	<tr>
+		<td>
+		<h1>LOG IN</h1>
+		</td>
+		
+		<td align="right">
+		<a class="btn btn-large btn-primary" href="<?php eh(url('thread/register')) ?>">SIGN UP</a>
+		</td>
+	</tr>
 </table>
+
+<?php
+if($invalid==1):?>
+	<div class="alert alert-block">
+	<h4 class="alert-heading">Login error!</h4>
+	<div>Invalid Account!
+	</div>
+	</div>
+<?php
+endif;?>
+
 <hr>
-<form method="post" action="<?php eh($login) ?>">
-<label>USERNAME:</label>
-<input type="text" class="span3" name="username" value="<?php eh(Param::get('username')) ?>"/>
-<label>PASSWORD:</label>
-<input type="text" class="span3" name="password" value="<?php eh(Param::get('password')) ?>"/>
-<br />
-<button type="submit" class="btn btn-primary">LOGIN</a>
+<form method="post" action="<?php eh(url('')) ?>">
+	<label>USERNAME:</label>
+	<input type="text" class="span3" name="username" value="<?php eh(Param::get('username')) ?>"/>
+	
+	<label>PASSWORD:</label>
+	<input type="password" class="span3" name="password" value="<?php eh(Param::get('password')) ?>"/>
+	
+	<br />
+	<input type="hidden" name="page_next" value="login_end">
+	<button type="submit" class="btn btn-primary">LOGIN</a>
 </form>
