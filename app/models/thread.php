eae499da7cd071256e5d42ecd467db759b75d5b7
Agnes Lumatao <a.lumatao@klab.com>
2013-10-04 10:08:38 +0800

    Simplify insert statements, simplify condition, modifiy existing user, new function for pagination and calculation of total page

--- app/models/thread.php
+++ app/models/thread.php
@@ -1,205 +1,198 @@
 <?php
 class Thread extends AppModel
 {
+	const LIMIT = 5, PAGE_MAX = 10, START_ONE = 9, START_TWO = 1, NEW_CREATE = 0;
+
 	//title must be 1-30 characters of length
 	public $validation = array(
-	'title' => array(
-	'length' => array(
-	'validate_between', 1, 30,
-	),
-	),
+        'title' => array(
+	    'length' => array(
+	    'validate_between', 1, 30,
+	    ),
+	    ),
 	);
 
-public function register(Account $account)
-{
-	if (!$account->validate() || $account->password!=$account->repassword) {
-	throw new ValidationException('invalid account');
-	}
+    public function register(Account $account)
+    {
+	    if (!$account->validate() OR
+            $account->password!=$account->repassword) {
+		    throw new ValidationException('invalid account');
+	    }
 	
-	$db = DB::conn();
-	$db->query('INSERT INTO account SET username = ?, password = ?',array($account->username, $account->password));
-	return $user_id = $db->lastInsertId();
-}
+	    $db = DB::conn();
+	    $params=array(
+            "username"=>$account->username,
+            "password"=>md5($account->password)
+        );
 
-public static function get($id)
-{
-	$db = DB::conn();
-	$row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
-	return new self($row);
-}
+        $db->insert("account",$params);
+	    return $db->lastInsertId();
+    }
 
-public static function getUser($username,$password)
-{
-	$db = DB::conn();
-	$row = $db->row('SELECT * FROM account WHERE username = ? && password = ?', array($username,$password));
-		//if account not found
-		if(!empty($row)){
-		return 1;
-		}
-}
+    public static function get($id)
+    {
+	    $db = DB::conn();
+	    $row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
+	    return new self($row);
+    }
 
-public static function getAccount($username, $password)
-{
-		
-	$db = DB::conn();
-	$login = $db->row("SELECT * FROM account WHERE username =? && password =?", array($username,$password));	
-	return $login;
+    public static function isUserExisting($username, $password)
+    {
+	    $db = DB::conn();
+	    $row = $db->row(
+            'SELECT id FROM account WHERE username = ? AND password = ?',
+            array($username,md5($password))
+        );
+
+		//if account exist
+		return $row? TRUE : FALSE;
+    }
+
+    public static function getAccount($username, $password)
+    {
+	    $db = DB::conn();
+	    $login = $db->row(
+            "SELECT id,username FROM account WHERE username = ? AND password = ?",
+            array($username,md5($password))
+        );
+	    return $login;
+    }
+
+    public static function getAll($user_id, $page)
+    {
+        $offset=($page-1)*Thread::LIMIT;
+        $threads = array();
+        $db = DB::conn();
+        $countThread= $db->value(
+            'SELECT count(id) FROM thread where user_id=?',
+            array($user_id)
+        );
 	
-}
-
-public static function getAll($user_id,$page)
-{
-	$limit=5;
-	$offset=($page-1)*$limit;
-	$threads = array();
-	$db = DB::conn();
-	$allThreads= $db->rows("SELECT * FROM thread where user_id=?", array($user_id));
-	$query="SELECT * FROM thread where user_id=? LIMIT ".$limit." OFFSET ".$offset;
-	$rows = $db->rows($query, array($user_id));
+	    $query="SELECT * FROM thread where user_id=? LIMIT "
+            .Thread::LIMIT." OFFSET ".$offset;
+	    $rows = $db->rows($query, array($user_id));
 
 		foreach ($rows as $v) {
-		$threads[] = array('id'=>$v['id'],'title'=>$v['title']);
+			$threads[] = array('id'=>$v['id'], 'title'=>$v['title']);
 		}
 
-	//for pagination
-	$countThread=count($allThreads);
-	$totalThread=count($rows);
-	$totalPage=ceil($countThread/$limit);
+        $totalPage = self::getTotalPage($countThread);
+        return self::pagination($page, $totalPage, $threads);
+    }
 
-	//maximum links to page number
-	$max=10;
+    public static function isThreadExisting($title, $user_id)
+    {
+        $threads = array();
+        $db = DB::conn();
+        $row = $db->row(
+            'SELECT id FROM thread where user_id = ? AND title = ?',
+            array($user_id,$title)
+        );
+
+        //check if thread title exists on the same account
+        return $row? TRUE : FALSE;
+    }
+
+    public function getComments($page)
+    {
+        $comments = array();
+        $db = DB::conn();
+        $countComment = $db->value(
+            'SELECT count(id) from comment where thread_id = ?',
+            array($this->id)
+        );
+
+	    $totalPage=self::getTotalPage($countComment);
 	
-	//number of pages to skip
-	$x=(floor($page/$max))*$max;
+        //thread goes to page where new comment is inserted
+        if ($page==Thread::NEW_CREATE) {
+            $page = $totalPage;
+        }
 
-	$remaining=$totalPage-$x;
-	//get the remaining pages
+        $offset = ($page-1)*Thread::LIMIT;
+        $query = "SELECT * FROM comment WHERE thread_id = ?
+            ORDER BY created ASC LIMIT ".Thread::LIMIT." OFFSET ".$offset;
+        $rows = $db->rows($query, array($this->id));
 
-	if($page==$x || $remaining>10){
-	$remaining=10;
-	}
-
-	//get the start of page numbers
-	if($page==$x){
-	$start=$x-9;
-	}
-
-	else{
-	$start=$x+1;
-	}
-	
-	
-	//get the end of page numbers
-	if($remaining>0){
-	$nums=$start+$remaining;
-
-	}
-	else{
-	$nums=$start+10;
-	}
-	
-	return array($threads,$totalThread,$totalPage,$nums,$start);
-}
-
-public static function checkThread($title,$user_id)
-{
-	$threads = array();
-	$db = DB::conn();
-	$rows = $db->rows('SELECT * FROM thread where user_id=? && title=?', array($user_id,$title));
-		//check if thread title exists on the same account
-		if(!empty($rows)){
-		return 1;
-		}
-}
-
-public function getComments($page)
-{
-	$limit=5;
-	$comments = array();
-	$db = DB::conn();
-	$allComments = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC', array($this->id));
-	
-	$countComment=count($allComments);
-	$totalPage=ceil($countComment/$limit);
-	//thread goes to page where new comment is inserted
-	if($page==0){
-	$page=$totalPage;
-	}
-	
-	
-	$offset=($page-1)*$limit;
-	$query="SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC LIMIT ".$limit." OFFSET ".$offset;
-	$rows = $db->rows($query, array($this->id));
-		foreach ($rows as $k => $v) {
-		$comments[] = array('created'=>$v['created'],'body'=>$v['body']);
+        foreach ($rows as $k => $v) {
+			$comments[] = array('created'=>$v['created'], 'body'=>$v['body']);
 		}
 
-	//for pagination
-	$totalComment=count($comments);
-
-	//maximum links to page number
-	$max=10;
-
-	//number of pages to skip
-	$x=(floor($page/$max))*$max;
 	
-	$remaining=$totalPage-$x;
-	//get the remaining pages
+	    return self::pagination($page, $totalPage, $comments);
 
-	if($page==$x || $remaining>10){
-	$remaining=10;
-	}
-	
-	
-	//get the start of page numbers
-	if($page==$x){
-	$start=$x-9;
-	}
+    }
 
-	else{
-	$start=$x+1;
-	}
+    public function create(Comment $comment)
+    {
+        $this->validate();
+        $comment->validate();
 
-	//get the end of page numbers
-	if($remaining>0){
-	$nums=$start+$remaining;
+        if ($this->hasError() OR $comment->hasError()) {
+            throw new ValidationException('invalid thread or comment');
+        }
+        date_default_timezone_set('Asia/Manila');
+        $date = date('Y-m-d H:i:s');
 
-	}
-	else{
-	$nums=$start+10;
-	}
+        $db = DB::conn();
+        $db->begin();
 
-	return array($comments,$totalComment,$totalPage,$nums,$page,$start);
-}
+        $params=array("user_id"=>$comment->user_id, "title"=>$this->title, "created"=>$date);
+	    $db->insert("thread", $params);;
+	    $this->id = $db->lastInsertId();
 
-public function create(Comment $comment)
-{
-	$this->validate();
-	$comment->validate();
-	if ($this->hasError() || $comment->hasError()) {
-	throw new ValidationException('invalid thread or comment');
-	}
+        // write first comment at the same time
+        $this->write($comment);
+        $db->commit();
+    }
 
-	$db = DB::conn();
-	$db->begin();
-	$db->query('INSERT INTO thread SET user_id= ?, title = ?, created = NOW()', array($comment->user_id, $this->title));
-	$this->id = $db->lastInsertId();
+    public function write(Comment $comment)
+    {
+        if (!$comment->validate()) {
+            throw new ValidationException('invalid comment');
+        }
+        date_default_timezone_set('Asia/Manila');
+        $date = date('Y-m-d H:i:s');
 
-	// write first comment at the same time
-	$this->write($comment);
-	$db->commit();
-}
+        $db = DB::conn();
+        $params=array("thread_id"=>$this->id, "body"=>$comment->body, "created"=>$date);
+        $db->insert("comment", $params);
+    }
 
-public function write(Comment $comment)
-{
 
-	if (!$comment->validate()) {
-	throw new ValidationException('invalid comment');
-	}
+    public static function getTotalPage($rowCount){
+        return ceil($rowCount/Thread::LIMIT);
+    }
 
-	$db = DB::conn();
-	$db->query('INSERT INTO comment SET thread_id = ?, body = ?, created = NOW()',array($this->id, $comment->body));
-}
+    public static function pagination($page, $totalPage, $array)
+    {
+	    $totalRow = count($array);
 
+        //number of pages to skip
+        $skip_page_count = (floor($page/Thread::PAGE_MAX))*Thread::PAGE_MAX;
+
+        //get the remaining pages
+	    $remaining = $totalPage-$skip_page_count;
+
+	    if ($page==$skip_page_count OR $remaining>Thread::PAGE_MAX) {
+	        $remaining=Thread::PAGE_MAX;
+	    }
+
+	    //get the start of page numbers
+	    if ($page==$skip_page_count) {
+	        $start = $skip_page_count-Thread::START_ONE;
+	    } else {
+	        $start = $skip_page_count+Thread::START_TWO;
+	    }
+
+        //get the end of page numbers
+        if ($remaining>0) {
+            $end=$start+$remaining;
+        } else {
+            $end=$start+Thread::PAGE_MAX;
+        }
+
+	    return array($array, $totalRow, $totalPage, $end, $start, $page);
+    }
 }
 ?>
