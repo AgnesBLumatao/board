ce7515f14c64485918974ccc8eedf831db52bc25
Agnes Lumatao <a.lumatao@klab.com>
2013-10-04 10:36:05 +0800

    Modify equation for comment number

--- app/views/thread/view.php
+++ app/views/thread/view.php
@@ -1,11 +1,13 @@
 <table width="100%">
 	<tr>
 		<td>
-		<h1><?php eh($thread->title) ?></h1>
+		    <h1><?php eh($thread->title) ?></h1>
 		</td>
 		
 		<td align="right">
-		<h5>WELCOME <?php eh($username) ?> <a class="btn btn-small btn-primary" href="<?php eh(url('thread/index')) ?>">LOG OUT</a></h5>
+		    <h5>WELCOME <?php eh($username) ?>
+                <a class="btn btn-small btn-primary" href="<?php eh(url('thread/index')) ?>">LOG OUT</a>
+            </h5>
 		</td>
 	</tr>
 	
@@ -14,9 +16,9 @@
 		</td>
 		
 		<td>
-		<a href="<?php eh(url('thread/home', array('page'=>1,'user_id' => $user_id, 'username'=>$username))) ?>">
-		back to thread
-		</a>
+            <a href="<?php eh(url('thread/home', array('page'=>1, 'user_id' => $user_id, 'username'=>$username))) ?>">
+            back to thread
+            </a>
 		</td>
 	</tr>
 </table>
@@ -24,27 +26,26 @@
 <br />
 <hr>
 
-<h5>PAGE <?php eh($page)?> OF <?php
-if($totalPage>0):
-eh($totalPage);
-
-else:
-eh('1');
-
-endif;?>
+<h5>
+    PAGE <?php eh($page)?> OF <?php
+    if ($totalPage>0) :
+        eh($totalPage);
+    else :
+        eh('1');
+    endif ?>
 </h5>
 
 <?php
 //show all comments
-for($x=0;$x<$totalComment;$x++):?>
+for ($x=0;$x<$totalComment;$x++) : ?>
 	<div class="comment">
 	
 		<div class="meta">
-		<?php eh($x+1) ?>:<?php eh($comments[$x]['created']) ?>
+		    <?php eh(($previousPage)*Thread::LIMIT+($x+1)) ?>:<?php eh($comments[$x]['created']) ?>
 		</div>
 		
 		<div>
-		<?php eh($comments[$x]['body']) ?>
+		    <?php eh($comments[$x]['body']) ?>
 		</div>
 	<br />
 	</div>
@@ -52,55 +53,54 @@ for($x=0;$x<$totalComment;$x++):?>
 endfor;
 
 //pagination
-if($totalPage>1):?>
-<table>
-<tr>
-	<?php
-	//link to previous 10 pages
-	if($start!=1):?>
-		<td>
-		<a href="<?php eh(url('thread/view', array('page'=>$start-10,'thread_id' => $thread->id, 'username'=>$username,'user_id'=>$user_id))) ?>">
-		&larr; previous
-		</a>
-		</td>
-	<?php
-	endif;
-	
-	//links for page number
-	for($x=$start;$x<$nums;$x++):
-		if($x!=$page):?>
-			<td>
-			<a href="<?php eh(url('thread/view', array('page'=>$x,'thread_id' => $thread->id, 'username'=>$username,'user_id'=>$user_id))) ?>">
-			<?php eh($x) ?>
-			</a>
-			</td>
-		
-		<?php
-		else:?>
-			<td>
-			<?php eh($x) ?>
-			</td>
-			<?php
-		
-		endif;
-	endfor;
+if ($totalPage>1) : ?>
+    <table>
+        <tr>
+            <?php
+            //link to previous 10 pages
+            if ($start!=1) : ?>
+                <td>
+                    <a href="<?php eh(url('thread/view', array('page'=>$previous, 'thread_id' => $thread->id, 'username'=>$username, 'user_id'=>$user_id))) ?>">
+                    &larr; previous
+                    </a>
+                </td>
+            <?php
+            endif;
 
-	//link to next 10 pages
-	if($start+9<$totalPage):?>
-	<td>
-	<a href="<?php eh(url('thread/view', array('page'=>$start+10,'thread_id' => $thread->id, 'username'=>$username,'user_id'=>$user_id))) ?>">
-	next &rarr;
-	</a>
-	</td>
-	<?php endif; ?>
+            //links for page number
+            for ($x=$start;$x<$nums;$x++) :
+                if($x!=$page) : ?>
+                    <td>
+                        <a href="<?php eh(url('thread/view', array('page'=>$x, 'thread_id' => $thread->id, 'username'=>$username, 'user_id'=>$user_id))) ?>">
+                        <?php eh($x) ?>
+                        </a>
+                    </td>
+                <?php
+                else : ?>
+                    <td>
+                        <?php eh($x) ?>
+                    </td>
+                <?php
+                endif;
+            endfor;
 
-</tr>
-</table>
+            //link to next 10 pages
+            if ($lastPage<$totalPage) : ?>
+                <td>
+                    <a href="<?php eh(url('thread/view', array('page'=>$next, 'thread_id' => $thread->id, 'username'=>$username, 'user_id'=>$user_id))) ?>">
+                    next &rarr;
+                    </a>
+                </td>
+            <?php
+            endif ?>
+
+        </tr>
+    </table>
 <?php
-endif; ?>
+endif ?>
 
 <hr>
-<form class="well" method="post" action="<?php eh(url('thread/write', array('page'=>0,'user_id'=>$user_id,'username'=>$username))) ?>">
+<form class="well" method="post" action="<?php eh(url('thread/write', array( 'page'=>0, 'user_id'=>$user_id, 'username'=>$username))) ?>">
 	<label>Comment</label>
 	<textarea name="body"><?php eh(Param::get('body')) ?></textarea>
 	
